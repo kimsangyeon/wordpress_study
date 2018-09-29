368d0af47f3d119efa3c6794a8f26cea8d7e6280
Kim Sangyeon <dho113@naver.com>
2018-09-29 21:27:36 +0900

    task/editor_task
    - editor init 코드 수정
    - image upload file 구현

--- wordpress-synap-editor/synap.php
+++ wordpress-synap-editor/synap.php
@@ -8,9 +8,9 @@ Author: kimsangyeon
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
-//function remove_post_default_editor() {
-//    remove_post_type_support( 'post', 'editor' );
-//}
+function remove_post_default_editor() {
+    remove_post_type_support('post', 'editor');
+}
 
 function enqueue_styles() {
     wp_register_style('synap_editor_css', plugin_dir_url( __FILE__ ) . 'css/synapeditor.css');
@@ -22,20 +22,58 @@ function enqueue_scripts() {
     wp_enqueue_script('synap_editor_js');
 }
 
+function synap_upload_files() {
+    if ($_FILES) {
+
+        // Let WordPress handle the upload.
+        $attachment_id = media_handle_upload( 'file', 0 );
+
+        if ( is_wp_error( $attachment_id ) ) {
+            // There was an error uploading the image.
+        } else {
+            // The image was uploaded successfully!
+            $file_path      = wp_get_attachment_url( $attachment_id );
+            $response       = new StdClass;
+            $response->link = $file_path;
+
+            echo stripslashes( json_encode( $response ) );
+        }
+    }
+    exit();
+    wp_die();
+}
+
 function initSynapEditor() {
     $post = get_post(get_the_ID());
     $content = apply_filters('the_content', $post->post_content);
+    $path = admin_url('admin-ajax.php');
 
-    wp_editor($content, 'content');
-
+    echo "<textarea id='content' name='content' style='display: none;'></textarea>" ;
     echo "<div id='editor-content' style='display: none;'>$content</div>" ;
     echo ("<script language=javascript>
-            window.editor = new SynapEditor('content');
-            window.editor.openHTML(document.getElementById('editor-content').innerHTML);
+            window.onload = function () {
+                if (SynapEditor) {
+                    window.editor = new SynapEditor('content', {
+                        importAPI: `./import.php`,
+                        imageUploadAPI: `$path`,
+                        videoUploadAPI: `$path`,
+                        fileUploadAPI: `$path`
+                    });
+                    window.editor.openHTML(document.getElementById('editor-content').innerHTML);
+                }
+            }
            </script>");
 }
 
-//add_action('init', 'remove_post_default_editor'); // plugin API init: WordPress가로드를 완료 한 후 모든 헤더가 전송되기 전에 발생합니다.
+add_action('init', 'remove_post_default_editor'); // plugin API init: WordPress가로드를 완료 한 후 모든 헤더가 전송되기 전에 발생합니다.
 add_action('admin_enqueue_scripts', 'enqueue_styles'); // admin_enqueue_scripts : 관리 페이지에 CSS 및 / 또는 Javascript 문서 세트를로드
 add_action('admin_enqueue_scripts','enqueue_scripts');
-add_action('edit_form_after_title', 'initSynapEditor' ); // 타이틀 필드의 뒤에서 발생합니다.
+
+// 워드프레스 2.8 이후로 wp_ajax_(action) 과 같은 hook이 생겼습니다.
+add_action('wp_ajax_synap_upload_files', 'synap_upload_files');
+
+// 로그인 하지 않은 사용자들에게 실행되는 ajax 액션 입니다.
+add_action('wp_ajax_nopriv_synap_upload_files', 'synap_upload_files');
+
+// 타이틀 필드의 뒤에서 발생합니다.
+add_action('edit_form_after_title', 'initSynapEditor' );
